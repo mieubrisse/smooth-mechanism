1f92d5adebbbb4a28cfe58620247d26798d0a75f
Kevin Today <ktoday@palantir.com>
2015-08-02 13:18:48 +0300

    Refactored smooth-mechanism code out into separate file

--- morning-review.py
+++ morning-review.py
@@ -12,6 +12,7 @@ from itertools import chain
 import os
 import io
 import re
+import smoothmechanism as sm
 
 # Constants related to sending the office tasks daily status email
 OFFICE_LIST_TITLE_PREFIX = "Office/"
@@ -26,91 +27,33 @@ OUTPUT_FILENAME_FORMAT_STR = '{}_tasks.md'
 CHECKPOINT_FILE_DATE_FORMAT = '%Y-%m-%d'
 CHECKPOINT_FILENAME_FORMAT_STR = '{}_tasks.checkpoint'
 
-CREDS_FILEPATH_ARGVAR = "creds_filepath"
+CONFIG_FILEPATH_ARGVAR = "config_filepath"
 EMAIL_TARGET_ARGVAR = "email_target_address"
 SMOOTH_MECHANISM_DIRPATH_ARGVAR = "smooth_mechanism_dirpath"
 
 DAILY_TASKS_DIRNAME = "daily-tasks"
 
-CREDS_FILE_CLIENT_ID_KEY = "client_id"
-CREDS_FILE_ACCESS_TOKEN_KEY = "access_token"
-
-SENSITIVE_TASK_FLAG = "!"
-TASK_FLAGS = [SENSITIVE_TASK_FLAG]
-# First group is time costing, second group is flags, third group is task contents
-MINUTE_COSTING_INDICATOR = 'm'
-HOUR_COSTING_INDICATOR = 'h'
-DAY_COSTING_INDICATOR = 'd'
-HOURS_IN_DAY = 5    # For the purposes of work costing, 1d = 5h. TODO Because this also needs to handle personal time as well, figure out a better method
-COSTING_INDICATOR_PARSER = '[mhdMHD]'   # TODO I probably *should* have this be somethign like MINUTE_COSTING_INDICATOR.lower(), MINUTE_COSTING_INDICATOR.upper()... but, meh
-TIME_COSTING_PARSER = '(\d+\.?\d*)({})'.format(COSTING_INDICATOR_PARSER)
-TASK_PARSER = '[\[(]?(\d+\.?\d*{})?[)\]]?\s*-?\s*([{}])*\s*(.*)'.format(COSTING_INDICATOR_PARSER, "".join(SENSITIVE_TASK_FLAG))
-TASK_COSTING_KEY = 'time_costing'
-TASK_FLAGS_KEY = 'flags'
-TASK_CONTENTS_KEY = 'contents'
 
 def parse_args():
+    ''' Parse command line arguments '''
     parser = argparse.ArgumentParser()
-    parser.add_argument(CREDS_FILEPATH_ARGVAR, metavar="<Wunderlist creds file>", help="JSON file to pull Wunderlist API creds from")
-    # TODO Make email address and output file optional args that only get written to if they exist
-    parser.add_argument(EMAIL_TARGET_ARGVAR, metavar="<destination email address>", help="email address to mail today's goals to")
-    parser.add_argument(SMOOTH_MECHANISM_DIRPATH_ARGVAR, metavar="<Smooth Mechanism dir>", help="root directory of all Smooth Mechanism tracking")
+    parser.add_argument(CONFIG_FILEPATH_ARGVAR, metavar="<Smooth Mechanism config file>", default="config.json", help="JSON file to pull Smooth Mechanism config from [default: config.json]")
     # TODO file name format
     # TODO other types of output pipes
     return vars(parser.parse_args())
 
 def validate_args(args):
+    ''' Validates command line args in one place '''
     # TODO Should probably do some Wunderlist connection validation stuff here
-    creds_filepath = args[CREDS_FILEPATH_ARGVAR]
+    config_filepath = args[CONFIG_FILEPATH_ARGVAR]
     try:
-        with open(creds_filepath, 'r'):
+        with open(config_filepath, 'r'):
             pass
     except IOError:
-        print "Unable to open {} for reading".format(creds_filepath)
+        print "Unable to open config file for reading: {}".format(config_filepath)
     # TODO Check to make sure the output dir exists
     # TODO Check to make sure the email is in proper format
 
-def parse_creds_file(creds_filepath):
-    ''' Parses the file holding Wunderlist access creds and returns the tuple (client ID, access token) '''
-    with open(creds_filepath) as creds_fp:
-        creds_obj = json.load(creds_fp)
-        return creds_obj[CREDS_FILE_CLIENT_ID_KEY], creds_obj[CREDS_FILE_ACCESS_TOKEN_KEY]
-
-def task_due_filter(task):
-    ''' Returns True if the task is due today or prior, False otherwise '''
-    if wunderclient.Task.due_date not in task:
-        return False
-    due_date = datetime.datetime.strptime(str(task[wunderclient.Task.due_date]), wunderclient.DATE_FORMAT).date()
-    return due_date <= datetime.date.today()
-
-def parse_task(task):
-    ''' Parses the task into an object containing the task's time costing in minutes, flags, and contents, or None if the task could not be parsed '''
-    title = task[wunderclient.Task.title]
-    match = re.match(TASK_PARSER, title)
-    if match is None:
-        return None
-    costing_str = match.group(1)
-    if costing_str is not None:
-        time, granularity = re.match(TIME_COSTING_PARSER, costing_str).groups()
-        time = float(time)
-        granularity_translator = {
-                MINUTE_COSTING_INDICATOR : lambda time : int(time),
-                HOUR_COSTING_INDICATOR : lambda time : int(time * 60),
-                DAY_COSTING_INDICATOR : lambda time : int(time * HOURS_IN_DAY * 60),
-                }
-        costing = granularity_translator[granularity.lower()](time)
-    else:
-        costing = 0
-    flags_str = match.group(2)
-    if flags_str is not None:
-        task_flags = [flag for flag in TASK_FLAGS if flag in flags_str]
-    else:
-        task_flags = []
-    return {
-            TASK_COSTING_KEY : costing,
-            TASK_FLAGS_KEY : task_flags,
-            TASK_CONTENTS_KEY : match.group(3),
-            }
 
 def work_email_formatter(tasks, comment=""):
     ''' 
@@ -125,7 +68,7 @@ def work_email_formatter(tasks, comment=""):
     office_tasks_list_of_lists = [ list_tasks for list_title, list_tasks in tasks.iteritems() if list_title.startswith(OFFICE_LIST_TITLE_PREFIX) ]
     office_tasks = list(chain(*office_tasks_list_of_lists))
     # TODO Make filtering functions like this actual functions
-    office_tasks = [task for task in office_tasks if SENSITIVE_TASK_FLAG not in parse_task(task)[TASK_FLAGS_KEY]]
+    office_tasks = [task for task in office_tasks if sm.SENSITIVE_TASK_FLAG not in sm.parse_task(task)[sm.TASK_FLAGS_KEY]]
     task_formatter = lambda task : u"\u2022 {}".format(task[wunderclient.Task.title])
     task_strs = map(task_formatter, office_tasks)
     return u'{}\n{}'.format(comment, u'\n'.join(task_strs))
@@ -177,17 +120,19 @@ def write_tasks_checkpoint_file(tasks, smooth_mechanism_dirpath):
 
 def generate_daily_tasks(args):
     ''' Pulls down today's due and overdue tasks from Wunderlist, writes them to file, and drafts a new email with office-only tasks in the default email client to the given email address '''
-    creds_filepath = args[CREDS_FILEPATH_ARGVAR]
-    email_addressee = args[EMAIL_TARGET_ARGVAR]
-    smooth_mechanism_dirpath = args[SMOOTH_MECHANISM_DIRPATH_ARGVAR]
+    config_filepath = args[CONFIG_FILEPATH_ARGVAR]
+    sm_config = sm.get_config(config_filepath)
+    client_id = sm_config[sm.ConfigKey.WUNDERLIST_CLIENT_ID]
+    access_token = sm_config[sm.ConfigKey.WUNDERLIST_ACCESS_TOKEN]
+    email_addressee = sm_config[sm.ConfigKey.DEST_EMAIL_ADDR]
+    smooth_mechanism_dirpath = os.path.abspath(os.path.expanduser(sm_config[sm.ConfigKey.SMOOTH_MECHANISM_DIRPATH]))
 
-    client_id, access_token = parse_creds_file(creds_filepath)
     client = wunderclient.WunderClient(access_token, client_id)
     wunder_lists = client.get_lists()
     todays_tasks = {}   # Dict mapping list name -> [tasks...] for lists with tasks due today (or before)
     for wunder_list in wunder_lists:
         list_tasks = client.get_tasks(wunder_list[wunderclient.List.id])
-        list_due_tasks = filter(task_due_filter, list_tasks)
+        list_due_tasks = filter(sm.task_due_filter, list_tasks)
         if len(list_due_tasks) > 0:
             list_title = wunder_list[wunderclient.List.title]
             todays_tasks[list_title] = list_due_tasks
--- smoothmechanism.py
+++ smoothmechanism.py
@@ -1,5 +1,8 @@
 import json
 import os.path
+import wunderclient
+import datetime
+import re
 
 def enum(**enums):
     return type('Enum', (), enums)
@@ -11,6 +14,56 @@ ConfigKey = enum(
     WUNDERLIST_ACCESS_TOKEN = "wunderlist_access_token",
     )
 
+SENSITIVE_TASK_FLAG = "!"
+TASK_FLAGS = [SENSITIVE_TASK_FLAG]
+# First group is time costing, second group is flags, third group is task contents
+MINUTE_COSTING_INDICATOR = 'm'
+HOUR_COSTING_INDICATOR = 'h'
+DAY_COSTING_INDICATOR = 'd'
+HOURS_IN_DAY = 5    # For the purposes of work costing, 1d = 5h. TODO Because this also needs to handle personal time as well, figure out a better method
+COSTING_INDICATOR_PARSER = '[mhdMHD]'   # TODO I probably *should* have this be somethign like MINUTE_COSTING_INDICATOR.lower(), MINUTE_COSTING_INDICATOR.upper()... but, meh
+TIME_COSTING_PARSER = '(\d+\.?\d*)({})'.format(COSTING_INDICATOR_PARSER)
+TASK_PARSER = '[\[(]?(\d+\.?\d*{})?[)\]]?\s*-?\s*([{}])*\s*(.*)'.format(COSTING_INDICATOR_PARSER, "".join(SENSITIVE_TASK_FLAG))
+TASK_COSTING_KEY = 'time_costing'
+TASK_FLAGS_KEY = 'flags'
+TASK_CONTENTS_KEY = 'contents'
+
 def get_config(config_filepath):
     with open(config_filepath) as config_fp:
         return json.load(config_fp)
+
+def task_due_filter(task):
+    ''' Returns True if the task is due today or prior, False otherwise '''
+    if wunderclient.Task.due_date not in task:
+        return False
+    due_date = datetime.datetime.strptime(str(task[wunderclient.Task.due_date]), wunderclient.DATE_FORMAT).date()
+    return due_date <= datetime.date.today()
+
+def parse_task(task):
+    ''' Parses the task into an object containing the task's time costing in minutes, flags, and contents, or None if the task could not be parsed '''
+    title = task[wunderclient.Task.title]
+    match = re.match(TASK_PARSER, title)
+    if match is None:
+        return None
+    costing_str = match.group(1)
+    if costing_str is not None:
+        time, granularity = re.match(TIME_COSTING_PARSER, costing_str).groups()
+        time = float(time)
+        granularity_translator = {
+                MINUTE_COSTING_INDICATOR : lambda time : int(time),
+                HOUR_COSTING_INDICATOR : lambda time : int(time * 60),
+                DAY_COSTING_INDICATOR : lambda time : int(time * HOURS_IN_DAY * 60),
+                }
+        costing = granularity_translator[granularity.lower()](time)
+    else:
+        costing = 0
+    flags_str = match.group(2)
+    if flags_str is not None:
+        task_flags = [flag for flag in TASK_FLAGS if flag in flags_str]
+    else:
+        task_flags = []
+    return {
+            TASK_COSTING_KEY : costing,
+            TASK_FLAGS_KEY : task_flags,
+            TASK_CONTENTS_KEY : match.group(3),
+            }
